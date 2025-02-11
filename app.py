421fbabe19e13cec143723997829266527f61f08
ravi1238-cm <ravisasta4@gmail.com>
2025-02-12 00:00:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -168,4 +168,4 @@ async def stream_chat(question: Question):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=10000)
